2ca703705be20e23993c847ec9720d35a1f7f060
HoangNguyendotcom <ndhoang7089@gmail.com>
2024-03-19 08:48:50 +0700

    get tracking files

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,8 @@
 import express, {Request, Response} from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
-
+import { User } from '../ models/user';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -15,7 +15,7 @@ router.post('/api/users/signup',[
     .isLength({min:4, max:20})
     .withMessage('Password must be between 4 an 20 characters.')
 ] , 
-(req: Request, res: Response) => {
+async (req: Request, res: Response) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw new RequestValidationError(errors.array());
@@ -23,10 +23,17 @@ router.post('/api/users/signup',[
 
   const {email, password} = req.body;
 
-  console.log('Creating a user....');
-  throw new DatabaseConnectionError();
-  res.send({});
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new BadRequestError("Email in use");
+  }
+  // Create a new user
+  const user = User.build({email, password});
+  // password is hashed in this step
+  await user.save();
 
+  // Response to app:
+  res.status(201).send(user);
 });
 
 export {router as signupRouter};
